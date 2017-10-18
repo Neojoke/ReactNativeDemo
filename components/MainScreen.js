5d2f12a9dbe4d89db5988c679af18891271b54e0
wupeng <wupeng@touker.com>
2017-10-18 17:25:25 +0800

    修改分割线

--- components/MainScreen.js
+++ components/MainScreen.js
@@ -1,17 +1,53 @@
-import {Button} from 'react-native'
+import {Button, FlatList, StyleSheet, Text, View} from 'react-native'
 import React, {Component} from 'react'
-
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    },
+    item: {
+        padding: 10,
+        fontSize: 18,
+        height: 44
+    }
+})
+function Separator(props) {
+    return (<View style={{
+        height: 1,
+        backgroundColor: 'black'
+    }}/>);
+}
 class MainScreen extends Component {
     static navigationOptions = {
         title: 'Index'
     };
     render() {
         const {navigate} = this.props.navigation;
-        return (<Button
-            title="Go to jane's profile"
-            onPress={() => {
-            navigate('Profile', {name: 'Jane'});
-        }}/>)
+        return (
+            <View style={styles.container}>
+                <FlatList
+                    ItemSeparatorComponent={Separator}
+                    data={[
+                    {
+                        key: 'Devin'
+                    }, {
+                        key: 'Jackson'
+                    }, {
+                        key: 'James'
+                    }, {
+                        key: 'Joel'
+                    }, {
+                        key: 'John'
+                    }, {
+                        key: 'Jillian'
+                    }, {
+                        key: 'Jimmy'
+                    }, {
+                        key: 'Julie'
+                    }
+                ]}
+                    renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}/>
+            </View>
+        )
     }
 }
 
